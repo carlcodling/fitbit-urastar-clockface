fb53e5c8c0d0013845207df9fb9dfe0df344d767
carlcodling <ccodling@gmail.com>
2021-08-17 18:54:05 +0100

    Initial layout/styling

--- app/index.js
+++ app/index.js
@@ -1,1 +1,27 @@
-console.log('Hello world!');
+import * as document from "document";
+
+import * as util from "../common/utils";
+
+const star1 = document.getElementById("star1");
+const star2 = document.getElementById("stat2");
+const star3 = document.getElementById("stat3");
+const star4 = document.getElementById("stat4");
+const star5 = document.getElementById("stat5");
+
+const SMALL_STAR_RADIUS = 40;
+
+let star2Coords = util.getPointOnCircle(342, 120, 168, 168);
+star2.x = parseInt(star2Coords.x)-SMALL_STAR_RADIUS;
+star2.y = parseInt(star2Coords.y)-SMALL_STAR_RADIUS;
+
+let star3Coords = util.getPointOnCircle(54, 120, 168, 168);
+star3.x = parseInt(star3Coords.x)-SMALL_STAR_RADIUS;
+star3.y = parseInt(star3Coords.y)-SMALL_STAR_RADIUS;
+
+let star4Coords = util.getPointOnCircle(126, 120, 168, 168);
+star4.x = parseInt(star4Coords.x)-SMALL_STAR_RADIUS;
+star4.y = parseInt(star4Coords.y)-SMALL_STAR_RADIUS;
+
+let star5Coords = util.getPointOnCircle(198, 120, 168, 168);
+star5.x = parseInt(star5Coords.x)-SMALL_STAR_RADIUS;
+star5.y = parseInt(star5Coords.y)-SMALL_STAR_RADIUS;
---
+++ common/utils.js
@@ -0,0 +1,36 @@
+export function getPointOnCircle(angle, radius, centerX, centerY){
+  let radians = degrees_to_radians(angle);
+  let x = (Math.cos(radians) * radius) + centerX;
+  let y = (Math.sin(radians) * radius) + centerY;
+  return {
+    "x":x,
+    "y":y
+  }
+}
+function degrees_to_radians(degrees)
+{
+  var pi = Math.PI;
+  return degrees * (pi/180);
+}
+
+// shift a hex colours RGB values by n (+n to lighten -n to darken)
+export function shadeColor(color, n) {
+
+    var R = parseInt(color.substring(1,3),16);
+    var G = parseInt(color.substring(3,5),16);
+    var B = parseInt(color.substring(5,7),16);
+
+    R += n;
+    G += n;
+    B += n;
+
+    R = (R<255)?R:255;
+    G = (G<255)?G:255;
+    B = (B<255)?B:255;
+
+    var RR = ((R.toString(16).length==1)?"0"+R.toString(16):R.toString(16));
+    var GG = ((G.toString(16).length==1)?"0"+G.toString(16):G.toString(16));
+    var BB = ((B.toString(16).length==1)?"0"+B.toString(16):B.toString(16));
+
+    return "#"+RR+GG+BB;
+}
